4c7995508de9e820c5f8ba7fb485c6abb25720d4
ali aslani <aliaslani.mm@gmail.com>
2023-06-25 20:33:54 +0330

    feat(base): docs

---
+++ vue-project/vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: path.join(__dirname, "../"),
+  }
+})
