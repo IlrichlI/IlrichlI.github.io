14839bc6b1176e2259085cf9c2c4086d8bb07d92
ali aslani <aliaslani.mm@gmail.com>
2023-06-27 16:37:41 +0330

    fix(pc): builded

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,55 +1,54 @@
 <script setup lang="ts">
-import { 
-  PersianDatePicker, 
+import {
+  PersianDatePicker,
   PersianCalendar,
   PersianButton,
   PersianCircleLoading,
-  PersianInput,
   PersianLinearLoading,
   PersianModal,
   PersianTooltip,
-  PersianTypography,
+  PersianTypography
 } from 'persian-components'
 </script>
 
 <template>
-  <div style="display: flex; flex-direction: column;gap: 40px;">
-    <div style="display: flex; gap: 40px;">
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+  <div style="display: flex; flex-direction: column; gap: 40px">
+    <div style="display: flex; gap: 40px">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianDatePicker />`}}</code>
         <PersianDatePicker />
       </div>
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{ `<PersianDatePicker modal />` }}</code>
         <PersianDatePicker modal />
       </div>
     </div>
-    <div style="display: flex; gap: 40px;">
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+    <div style="display: flex; gap: 40px">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianLinearLoading show />`}}</code>
         <PersianLinearLoading :show="true" />
       </div>
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianCircleLoading />`}}</code>
         <PersianCircleLoading />
       </div>
     </div>
-    <div style="display: flex; gap: 40px;">
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+    <div style="display: flex; gap: 40px">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianModal />`}}</code>
         <PersianModal modal-id="modal" />
       </div>
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianButton active>دکمه</PersianButton>`}}</code>
         <PersianButton active>دکمه</PersianButton>
       </div>
     </div>
-    <div style="display: flex; gap: 40px;">
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+    <div style="display: flex; gap: 40px">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianTypography text="چهارشنبه ها" />`}}</code>
         <PersianTypography text="چهارشنبه ها" />
       </div>
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianTooltip />`}}</code>
         <PersianTooltip>
           <template #content>متن</template>
@@ -57,15 +56,15 @@ import {
         </PersianTooltip>
       </div>
     </div>
-    <div style="display: flex; gap: 40px;">
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+    <div style="display: flex; gap: 40px">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianInput /> readonly`}}</code>
         <PersianInput />
       </div>
-      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px;">
+      <div style="width: 50%; display: flex; flex-direction: column; margin-bottom: 40px">
         <code>{{`<PersianCalendar />`}}</code>
         <PersianCalendar />
       </div>
-    </div>  
+    </div>
   </div>
 </template>
--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -4,11 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { PersianComponents } from 'persian-components'
-
+import 'persian-components/style.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(PersianComponents)
 
-app.mount('#app')
+app.mount('#appp')
